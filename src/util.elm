440e9348d0b60fd2b47fcc35d17e2865c49a400a
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2018-07-20 22:35:39 +0200

    remove unused `resultToTask` function

--- src/util.elm
+++ src/util.elm
@@ -1,13 +1,6 @@
 module Util exposing (..)
 
-import Task exposing (Task, succeed, fail)
 import Date exposing (Date, year, month, day)
-
-resultToTask : Result err val -> Task err val
-resultToTask res =
-    case res of
-        Ok val -> succeed val
-        Err err -> fail err
 
 sameDate : Date -> Date -> Bool
 sameDate a b =
